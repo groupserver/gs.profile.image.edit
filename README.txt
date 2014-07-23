520e472ee9decae732748ca534093e7bbf58de8c
wbushey <wbushey@gmail.com>
2014-07-23 13:54:45 -0500

    Initial Import

---
+++ README.txt
@@ -0,0 +1,37 @@
+==============================
+``gs.profile.image.edit``
+==============================
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Provides a page for a user to set/edit her profile image
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Author: `Bill Bushey`_
+:Contact: `Michael JasonSmith`_ <mpj17@onlinegroups.net>
+:Date: 2014-07-23
+:Organization: `E-Democracy`_ & `OnlineGroups.Net`_
+:Copyright: This document is licensed under a
+  `Creative Commons Attribution-Share Alike 3.0 License`_
+  by `E-Democracy`_.
+
+Introduction
+===========
+
+This product provides the `GroupServer`_ profile page /image.html, which allows 
+a user to upload a profile image, and select whether or not to display the 
+profile image.
+
+Resources
+=========
+
+- Code repository: https://source.iopen.net/groupserver/gs.profile.image.edit 
+- Questions and comments to http://groupserver.org/groups/development
+- Report bugs at https://redmine.iopen.net/projects/groupserver
+
+.. _GroupServer: http://groupserver.org/
+.. _GroupServer.org: http://groupserver.org/
+.. _E-Democracy: http://e-democracy.org/
+.. _OnlineGroups.Net: https://onlinegroups.net
+.. _Bill Bushey: http://groupserver.org/p/wbushey
+.. _Michael JasonSmith: http://groupserver.org/p/mpj17
+.. _Creative Commons Attribution-Share Alike 3.0 License:
+   http://creativecommons.org/licenses/by-sa/3.0/
---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding=utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+import os
+from setuptools import setup, find_packages
+from version import get_version
+
+version = get_version()
+
+setup(
+    name='gs.profile.image.edit',
+    version=version,
+    description="Provides a page for a user to set/edit her profile image",
+    long_description=open("README.txt").read() + "\n" +
+    open(os.path.join("docs", "HISTORY.txt")).read(),
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers for values
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='',
+    author='Bill Bushey',
+    author_email='bill.bushey@e-democracy.org',
+    url='http://groupserver.org/',
+    license='ZPL 2.1',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['gs', 'gs.profile', 'gs.profile.image'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,)
