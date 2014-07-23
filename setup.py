73182b6f3950368b069a880402217d09dc28fc53
wbushey <wbushey@gmail.com>
2014-07-23 15:37:42 -0500

    Set dependencies

--- setup.py
+++ setup.py
@@ -36,6 +36,15 @@ setup(
     install_requires=[
         'setuptools',
         'Products.CustomUserFolder',
+        'Products.GSProfile',
+        'Products.XWFCore',
+        'gs.core',
+        'gs.profile.base',
+        'zope.app.apidoc',
+        'zope.component',
+        'zope.formlib',
+        'zope.interface',
+        'zope.schema',
         # -*- Extra requirements: -*-
     ],
     entry_points="""
